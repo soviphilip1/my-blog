3c3bfe2ffed2500005eda45dd43f80fef7fcacca
Philip Sovi <psovi@apple.com>
2020-08-02 13:15:28 -0700

    First Commit

---
+++ src/build/precache-manifest.318e0e41ef28eb1fb7361cd49bd8523a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec2fdb5a2dd2ae7153cc7dc50f7c1db9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1a35d83fbccb306d905d",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "61b81a605b7c806e89e4",
+    "url": "/static/js/2.d4df99f9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d4df99f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a35d83fbccb306d905d",
+    "url": "/static/js/main.326acb89.chunk.js"
+  },
+  {
+    "revision": "8931283fa7532b66ee7f",
+    "url": "/static/js/runtime-main.fca178c7.js"
+  }
+]);
